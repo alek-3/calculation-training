fc15e6fb37d641a6e641170e895a38cffd6d8970
hkdnet <hkdnet@users.noreply.github.com>
2017-05-28 02:10:38 +0900

    Add examples

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ var app = express()
 
 app.use(express.static('public'))
 
+// handling GET
+app.get('/test', function(req, res) {
+  res.send("GET /test");
+});
+// handling POST
+app.post('/test', function(req, res) {
+  res.send("POST /test");
+});
+
+// Use pug as our template engine
+app.set('view engine', 'pug');
+// templates are in ./views
+app.set('views', './views');
+app.get('/foo', function(req, res) {
+  // get name from URL parameter
+  // http://localhost:3000/foo?name=foo
+  let name = req.query['name'] || 'nanasi';
+  res.render('foo', {title: 'greeting', message: 'Hello ' + name + '-san.'});
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
