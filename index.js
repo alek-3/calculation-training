6e2697109311612a95d41a2a0269d503d80ce644
alek-3 <alek-3@users.noreply.github.com>
2017-05-28 13:22:00 +0000

    make init login page and home page.

--- index.js
+++ index.js
@@ -16,12 +16,23 @@ app.post('/test', function(req, res) {
 app.set('view engine', 'pug');
 // templates are in ./views
 app.set('views', './views');
+
 app.get('/foo', function(req, res) {
   // get name from URL parameter
   // http://localhost:3000/foo?name=foo
+  let kibun = req.query['kibun'] || 'so-so';
+  res.render('foo', {message2: 'You are '+kibun+ ' now.'} );
+
   let name = req.query['name'] || 'nanasi';
   res.render('foo', {title: 'greeting', message: 'Hello ' + name + '-san.'});
+  
 });
+
+app.post('/home', function(req, res) {
+  let name = req.query['name'] || 'nanasi';
+  res.render('home', {title: 'Home', message: 'ようこそ、' + name + 'さん。'});
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
