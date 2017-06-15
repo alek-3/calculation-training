075761b37f3ab36df5ca5d2b745d9ef8d7dc61d6
hkdnet <hkdnet@users.noreply.github.com>
2017-06-16 00:09:30 +0900

    lint

--- src/app.js
+++ src/app.js
@@ -1,55 +1,55 @@
-var express = require('express')
-var session = require('express-session');
-var app = express()
-var bodyParser = require('body-parser');
+var express = require("express");
+var session = require("express-session");
+var app = express();
+var bodyParser = require("body-parser");
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use(session({
-  secret: 'keyboard cat',
+  secret: "keyboard cat",
   resave: false,
   saveUninitialized: true,
 //  cookie: { secure: true }
 })); // For session
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // handling GET
-app.get('/test', function(req, res) {
+app.get("/test", function(req, res) {
   res.send("GET /test");
 });
 // handling POST
-app.post('/test', function(req, res) {
+app.post("/test", function(req, res) {
   res.send("POST /test");
 });
 
 // Use pug as our template engine
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 // templates are in ./views
-app.set('views', './views');
+app.set("views", "./views");
 
-const fooHandler = require('./handlers/foo.js');
+const fooHandler = require("./handlers/foo.js");
 
-app.get('/foo', fooHandler);
+app.get("/foo", fooHandler);
 
-app.post('/home', function(req, res) {
-  let name = req.body.username || 'nanasi';
-  res.render('home', {title: 'Home', message: 'ようこそ、' + name + 'さん。'});
+app.post("/home", function(req, res) {
+  let name = req.body.username || "nanasi";
+  res.render("home", {title: "Home", message: "ようこそ、" + name + "さん。"});
 });
 
-app.get('/signin', function(req, res) {
-  res.render('signin');
+app.get("/signin", function(req, res) {
+  res.render("signin");
 });
 
-app.post('/index.html', function(req, res) {
-  console.log('Enter into index.html');
-  res.render('/index.html');
+app.post("/index.html", function(req, res) {
+  console.log("Enter into index.html");
+  res.render("/index.html");
 });
 
-app.post('/game', function(req, res) {
+app.post("/game", function(req, res) {
   // Get type of game  
   if(req.body.calctype && req.body.difficulty){
-    console.log('Set session');
+    console.log("Set session");
     req.session.calctype =  req.body.calctype;
     req.session.difficulty =  req.body.difficulty;
     req.session.questionNum = 1;
@@ -81,7 +81,7 @@ app.post('/game', function(req, res) {
     let result = TimeGetTimeString(time);
     console.log(result);
 
-    res.render('results', {result:'結果：'+result});
+    res.render("results", {result:"結果："+result});
   }
 
   let firstnum = 1 + Math.floor( Math.random() * 9 ) ;
@@ -89,24 +89,24 @@ app.post('/game', function(req, res) {
   req.session.firstnum = firstnum;
   req.session.secondnum = secondnum;
   
-  res.render('game', {message: calctype, message2: 'むずかしさ：' + difficulty, 
-  qcount:'Q.'+num,  message3: firstnum+' + '+secondnum+' = ?'});
+  res.render("game", {message: calctype, message2: "むずかしさ：" + difficulty, 
+    qcount:"Q."+num,  message3: firstnum+" + "+secondnum+" = ?"});
 });
 
 function TimeGetTimeString(time){
 
-	var milli_sec = time % 1000;
-	time = (time - milli_sec) / 1000;
-	var sec = time % 60;
-	time = (time - sec) / 60;
-	var min = time % 60;
-	var hou = (time - min) / 60;
+  var milli_sec = time % 1000;
+  time = (time - milli_sec) / 1000;
+  var sec = time % 60;
+  time = (time - sec) / 60;
+  var min = time % 60;
+  var hou = (time - min) / 60;
 
-	// 文字列として連結
-	return hou  + ":" +
-		((min < 10) ? "0" : "") + min + ":" +
-		((sec < 10) ? "0" : "") + sec + "." +
-		((milli_sec < 100) ? "0" : "") + ((milli_sec < 10) ? "0" : "") + milli_sec;
+  // 文字列として連結
+  return hou  + ":" +
+  ((min < 10) ? "0" : "") + min + ":" +
+  ((sec < 10) ? "0" : "") + sec + "." +
+  ((milli_sec < 100) ? "0" : "") + ((milli_sec < 10) ? "0" : "") + milli_sec;
 }
 
 
