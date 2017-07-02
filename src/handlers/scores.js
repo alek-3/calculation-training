d8ad90f9642720c9bd582ad36fc66d2eb9006f10
alek-3 <alek-3@users.noreply.github.com>
2017-07-02 08:54:20 +0000

    Connect to result table

--- src/handlers/scores.js
+++ src/handlers/scores.js
@@ -1,3 +1,14 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host : "127.0.0.1",
+  user : "root",
+  password : "password",
+  database: "calc_training"
+});
+
 module.exports = function(req, res){
-  res.render("scores");
+  connection.query("SELECT player_name, result_time, game_type_id, difficulty_id, playing_date FROM result ORDER BY result_time;", function (error, results) {
+    if (error) { console.log("err: " + error); } 
+    res.render("scores",{data: results});
+  });
 };
