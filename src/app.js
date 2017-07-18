eab6eeefdb6ee7068c02d187cd91ffb28825671f
alek-3 <alek-3@users.noreply.github.com>
2017-07-18 22:59:26 +0000

    Set select box for filtering of the table

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   user : "root",
   password : "password",
   database: "calc_training",
-  timezone:"jst"
+  timezone: "jst"
 });
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
