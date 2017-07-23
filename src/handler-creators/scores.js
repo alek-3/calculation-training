2b86a875a497b99d87435e879f73bb42eb9a7a58
alek-3 <alek-3@users.noreply.github.com>
2017-07-23 12:25:00 +0000

    Try to get data using query parameter

--- src/handler-creators/scores.js
+++ src/handler-creators/scores.js
@@ -1,9 +1,18 @@
-module.exports = function(options) {
+module.exports = function(options, req) {
+  let type = req.query["calctype"]; // ここでエラーになる。
+  let difficulty = req.query["difficulty"];
+
   const conn = options.connection;
   const query = `
   SELECT
-    player_name, result_time, game_type_id, difficulty_id, playing_date
-  FROM result
+    a.player_name, a.result_time, a.game_type_id, a.difficulty_id, a.playing_date
+  FROM result a
+  INNER JOIN game_type b
+  ON a.game_type_id = b.game_type_id
+  INNER JOIN difficulty c
+  ON a.difficulty_id = c.difficulty_id
+  WHERE b.game_type_name = '`+type+`'
+  AND c.difficulty_name = '`+difficulty+`'
   ORDER BY result_time;
   `;
   return function(req, res) {
