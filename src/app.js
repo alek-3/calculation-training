91661107891cbbf6416a5384c0f5199a56c206e2
alek-3 <alek-3@users.noreply.github.com>
2017-08-26 07:47:35 +0000

    environment variables

--- src/app.js
+++ src/app.js
@@ -4,12 +4,7 @@ var app = express();
 var bodyParser = require("body-parser");
 const { Client } = require("pg");
 const client = new Client({
-  host : process.env.DB_HOST,
-  port : process.env.DB_PORT,
-  user : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  timezone: process.env.DB_TIMEZONE,
+  connectionString: process.env.DATABASE_URL,
 });
 client.connect();
 app.use(bodyParser.json()); // for parsing application/json
